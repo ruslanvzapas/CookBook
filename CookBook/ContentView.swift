0b7ae89a6ea90f5da88e841929212ba0d2d3192f
Ruslan Vavulskyi-Zapasnyk <124261674+ruslanvzapas@users.noreply.github.com>
2025-01-11 00:22:28 +0200

    Initial Commit

---
+++ CookBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookBook
+//
+//  Created by Ruslan Vavulskyi-Zapasnyk on 11.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Ruslan Vavulskyi-Zapasnyk on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
